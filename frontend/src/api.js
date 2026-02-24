c39fbaa9719846565b103059af762757d2cbc870
srihariharan-07 <srihariharan.n25@gmail.com>
2026-02-24 06:49:55 +0530

    update api url to production

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
-
+const API = axios.create({ baseURL: 'https://healthplus-gzh5.onrender.com/api' });
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem('token');
   if (token) req.headers.Authorization = `Bearer ${token}`;
